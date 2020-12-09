4cc2feb963a8734133545372ed3a1412378e6e21
DTUGYILMAZ <ugur.yilmaz@digiturk.com.tr>
2020-12-09 16:14:53 +0300

    Mocks and Spies

--- script2.test.js
+++ script2.test.js
@@ -15,3 +15,21 @@ it("calls swapi to get people with a promise", () => {
     expect(data.results.length).toBeGreaterThan(5);
   });
 });
+
+it('getPeople return count and results', () => {
+  const mockFetch = jest.fn()
+    .mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve({
+        count: 87,
+        results: [0, 1, 2, 3, 4, 5, 6]
+      })
+    }))
+
+  expect.assertions(4)
+  return swapi.getPeoplePromise(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people');
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  })
+})
